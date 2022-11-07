01ea55c235c64402854a0a4290498da144a7f856
alemonacoo <ale.monaco@outlook.com>
2022-11-07 12:38:04 +0100

    creating trains migration + table

---
+++ database/migrations/2022_11_07_105347_create_trains_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            /*
+            Azienda
+            Stazione di partenza
+            Stazione di arrivo
+            Orario di partenza
+            Orario di arrivo
+            Codice Treno
+            Numero Carrozze
+            In orario
+            Cancellato
+            */
+            $table->string('azienda');
+            $table->string('stazione_partenza', 30);
+            $table->string('stazione_arrivo', 30);
+            $table->dateTime('orario_partenza');
+            $table->dateTime('orario_arrivo');
+            $table->string('codice_treno', 10)->unique();
+            $table->tinyInteger('carrozze')->unsigned();
+            $table->boolean('in_orario')->default(true);
+            $table->boolean('cancellato')->default(false);
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
